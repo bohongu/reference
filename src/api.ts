726ae18477ee6219b2d34c06c2b42de2d14451d5
bohongu <jaehong2782@naver.com>
2022-12-13 12:18:04 +0900

    home screen

---
+++ src/api.ts
@@ -0,0 +1,24 @@
+const API_KEY = "fc4afa5ff9ec346ff376ee2b5da182ac";
+const BASE_PATH = "https://api.themoviedb.org/3";
+
+interface IMovie {
+  id: number;
+  backdrop_path: string;
+  paster_path: string;
+  title: string;
+  overview: string;
+}
+
+export interface IGetMoviesResult {
+  dates: { maximum: string; minimum: string };
+  page: number;
+  results: IMovie[];
+  total_pages: number;
+  total_results: number;
+}
+
+export function getMovies() {
+  return fetch(`${BASE_PATH}/movie/now_playing?api_key=${API_KEY}`).then(
+    (response) => response.json()
+  );
+}
